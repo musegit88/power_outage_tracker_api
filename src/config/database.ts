83963b86458b130a7340bbeda2b40901379abff4
Muse <musemy8484@gmail.com>
2026-03-28 23:59:27 +0300

    refactor: add prisma logs during development

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,7 +9,13 @@ const connectionString = process.env.DATABASE_URL;
 const adapter = new PrismaPg({
   connectionString,
 });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "info", "warn", "error"]
+      : ["error"],
+});
 
 process.on("beforeExit", async () => {
   await prisma.$disconnect();
